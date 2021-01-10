bf0d05007ed6b209df0eda19d6682840d0d7aa60
Sylvain <sylvainlosey@gmail.com>
2021-01-10 12:17:57 +0100

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1. Do the laundry" for row in rows))
+        self.assertTrue(
+            any(row.text == "1. Do the laundry" for row in rows),
+            "New to-do item did not appear in table",
+        )
 
         # There is still a text box inviting him to enter another todo, he enters "Lolz"
         self.fail("Finish the test!")
